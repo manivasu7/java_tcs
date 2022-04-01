f8c4b58fb471ca8dbf8e8d63c6a17ba81086ed38
cpenriqu <cpenriqu@pichincha.com>
2022-04-01 13:51:29 -0500

    OK

--- src/main/java/com/pichincha/java_tcs_cpenriqu/repository/MovementRepo.java
+++ src/main/java/com/pichincha/java_tcs_cpenriqu/repository/MovementRepo.java
@@ -1,5 +1,13 @@
 package com.pichincha.java_tcs_cpenriqu.repository;
 
-public interface MovementRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pichincha.java_tcs_cpenriqu.model.Movement;
+
+
+
+@Repository
+public interface MovementRepo extends JpaRepository<Movement, Integer> {
 
 }
