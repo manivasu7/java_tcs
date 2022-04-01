af5566f86fdf12da777c83c4e7a3d25d07590a04
cpenriqu <cpenriqu@pichincha.com>
2022-04-01 11:33:42 -0500

    Inicio

---
+++ src/main/java/com/pichincha/java_tcs_cpenriqu/JavaTcsCpenriquApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.java_tcs_cpenriqu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTcsCpenriquApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTcsCpenriquApplication.class, args);
+	}
+
+}
