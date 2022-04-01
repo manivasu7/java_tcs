c6543b045821d17e9f59dddda88a677fada86723
ajitbiswal21 <ajitbiswal1@gmail.com>
2022-04-01 12:15:19 -0500

    initial

---
+++ movements/src/test/java/com/pichincha/bcb/movements/MovementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.bcb.movements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
